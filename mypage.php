76fda802a46dd0ba481fc367851754f529e58e0f
MayumiOzawa <mymozawa@gmail.com>
2015-12-23 13:41:06 +0800

    12/22作業分

--- mypage.php
+++ mypage.php
@@ -5,87 +5,78 @@ date_default_timezone_set('Asia/Manila');
 require('dbconnect.php');
 
 if (!empty($_POST)){
-//エラー項目の確認している。
-    if($_POST['title']==''){
-        $error['title'] ='blank';
-    }
+  //エラー項目の確認している。
+  if($_POST['title']==''){
+      $error['title'] ='blank';
+  }
     if($_POST['day']==''){
         $error['day'] ='blank';
     }else{  
-        if(!preg_match('/^[0-9]{4}\/[0-9]{1,2}\/[0-9]{1,2}$/', $_POST['day'])) {
+      if(!preg_match('/^[0-9]{4}\/[0-9]{1,2}\/[0-9]{1,2}$/', $_POST['day'])) {
          
         }else{ 
             list($year, $momth, $day) = preg_split("/\-/", $_POST['day']);
             if(!checkdate($momth, $day, $year)){
-            
             }
         
         }
-
-
-    }
-    if($_POST['time']==''){
-        $error['time'] ='blank';
-    }
-    if($_POST['place']==''){
-        $error['place'] ='blank';
-    }
-    if($_POST['remark']==''){
-        $error['remark'] ='blank';
-    }
+      }
+      if($_POST['time']==''){
+          $error['time'] ='blank';
+      }
+      if($_POST['place']==''){
+          $error['place'] ='blank';
+      }
+      if($_POST['remark']==''){
+          $error['remark'] ='blank';
+      }
     
-    //ファイルの拡張子チェック
-    // $fileName = $_FILES($_POST['picture']);
-    // if(!empty($fileName)){
-    //     $ext = substr($fileName, -3);
-    //     if ($ext !='jpg' && $ext !='gif'&& $ext !='jpeg'&& $ext !='png'){
-    //         $error['picture'] ='type';
-    
-    if (empty($error)) {
-    //画面をアップロードする。date('YmdHis')のファイルをアップロードしているときの日時。例えば、20151104151539など。時間差を利用し誰のファイルかどうか区分したいため。
-    //$_FILESスーパーグローバル変数
-    $image = date('YmdHis') . $_FILES['image'];
-     move_uploaded_file($_FILES['image']['tmp_name'],'assets/img/plan_img'.$image);
+      //ファイルの拡張子チェック
+      // $fileName = $_FILES($_POST['picture']);
+      // if(!empty($fileName)){
+      //     $ext = substr($fileName, -3);
+      //     if ($ext !='jpg' && $ext !='gif'&& $ext !='jpeg'&& $ext !='png'){
+      //         $error['picture'] ='type';
+      
+      if (empty($error)) {
+        //画面をアップロードする。date('YmdHis')のファイルをアップロードしているときの日時。例えば、20151104151539など。時間差を利用し誰のファイルかどうか区分したいため。
+        //$_FILESスーパーグローバル変数
+        $image = date('YmdHis') . $_FILES['image'];
+         move_uploaded_file($_FILES['image']['tmp_name'],'assets/img/plan_img'.$image);
 
-     $_SESSION['join'] = $_POST;
-     $_SESSION['join']['image'] = $image;
-     //画面遷移。画面移動という意味。
-     header('Location:mypage.php');
-     exit();
-    }
-
-
-    // var_dump($_FILES);
-
+         $_SESSION['join'] = $_POST;
+         $_SESSION['join']['image'] = $image;
+         //画面遷移。画面移動という意味。
+         header('Location:mypage.php');
+         exit();
+      }
     } 
 
 
-
-
-
-
-    
-
     if(!empty($_POST)) {
-    //登録処理をする
-    $sql = sprintf('INSERT INTO plans SET category_id=%d, user_id=%d, title="%s", day="%s",time=%d,place="%s", remark="%s",created=NOW()',
-    
-    mysqli_real_escape_string($db,$_POST['category_id']),
-    mysqli_real_escape_string($db,$_POST['user_id']),
-    mysqli_real_escape_string($db,$_POST['title']),
-    mysqli_real_escape_string($db,$_POST['day']),
-    mysqli_real_escape_string($db,$_POST['time']),
-    mysqli_real_escape_string($db,$_POST['place']),
-    mysqli_real_escape_string($db,$_POST['remark']),
-    mysqli_real_escape_string($db,$_POST['created']),
+      //登録処理をする
+      $sql = sprintf('INSERT INTO plans SET category_id=%d, user_id=%d, title="%s", day="%s",time=%d,place="%s", remark="%s",created=NOW()',   
+      mysqli_real_escape_string($db,$_POST['category_id']),
+      mysqli_real_escape_string($db,$_POST['user_id']),
+      mysqli_real_escape_string($db,$_POST['title']),
+      mysqli_real_escape_string($db,$_POST['day']),
+      mysqli_real_escape_string($db,$_POST['time']),
+      mysqli_real_escape_string($db,$_POST['place']),
+      mysqli_real_escape_string($db,$_POST['remark']),
+      mysqli_real_escape_string($db,$_POST['created']),
+      date('Y-m-d H:i:s')
+      );
 
-    date('Y-m-d H:i:s')
-    );
+      mysqli_query($db,$sql)or die(mysqli_error($db));
+      header('Location: mypage.php');
+      exit();
+    }
 
-    mysqli_query($db,$sql)or die(mysqli_error($db));
-    header('Location: mypage.php');
-    exit();
-}
+  //投稿を取得する
+  $sql = sprintf('SELECT pl.*, u.nickname, u.upicture FROM plans pl LEFT JOIN users u ON u.id=pl.user_id WHERE u.id=%d AND pl.status<>2 ORDER BY pl.created DESC LIMIT %d, %d',
+           $category_id, $start, $limit_qty);
+  $myplans = mysqli_query($db, $sql) or die(mysqli_error($db));
+
 
 ?>
 
@@ -116,156 +107,45 @@ if (!empty($_POST)){
     <link href="assets/css/mypage.css" rel="stylesheet">
     
     
-    <!-- JavaScripts needed at the beginning
-    ================================================== -->
-    <script type="text/javascript" src="http://alvarez.is/demo/marco/assets/js/twitterFetcher_v10_min.js"></script>
-
-    <!-- MAP SCRIPT - ALL CONFIGURATION IS PLACED HERE - VIEW OUR DOCUMENTATION FOR FURTHER INFORMATION -->
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyASm3CwaK9qtcZEWYa-iQwHaGi3gcosAJc&sensor=false"></script>
-    <script type="text/javascript">
-        // When the window has finished loading create our google map below
-        google.maps.event.addDomListener(window, 'load', init);
-    
-        function init() {
-            // Basic options for a simple Google Map
-            // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-            var mapOptions = {
-                // How zoomed in you want the map to start at (always required)
-                zoom: 11,
-                
-                scrollwheel: false,
-
-                // The latitude and longitude to center the map (always required)
-                center: new google.maps.LatLng(40.6700, -73.9400), // New York
-
-                // How you would like to style the map. 
-                // This is where you would paste any style found on Snazzy Maps.
-                styles: [	{		featureType:'water',		stylers:[{color:'#74c9be'},{visibility:'on'}]	},{		featureType:'landscape',		stylers:[{color:'#f2f2f2'}]	},{		featureType:'road',		stylers:[{saturation:-100},{lightness:45}]	},{		featureType:'road.highway',		stylers:[{visibility:'simplified'}]	},{		featureType:'road.arterial',		elementType:'labels.icon',		stylers:[{visibility:'off'}]	},{		featureType:'administrative',		elementType:'labels.text.fill',		stylers:[{color:'#444444'}]	},{		featureType:'transit',		stylers:[{visibility:'off'}]	},{		featureType:'poi',		stylers:[{visibility:'off'}]	}]
-            };
-
-            // Get the HTML DOM element that will contain your map 
-            // We are using a div with id="map" seen below in the <body>
-            var mapElement = document.getElementById('map');
-
-            // Create the Google Map using out element and options defined above
-            var map = new google.maps.Map(mapElement, mapOptions);
-        }
-    </script>
-    
-    
-    <!-- Main Jquery & Hover Effects. Should load first -->
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="assets/js/hover_pack.js"></script>
-    
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
 
 
-<div class="navbar navbar-inverse navbar-static-top hidden-print">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <i class="fa fa-bars fa-lg"></i>
-      </button>
-      <div class="logo">
-        <a href="face_to_face.php"><img src="assets/img/logo.png" style="height:70px;"><i class="fas fas-logo"></i><a>
+  <div class="navbar navbar-inverse navbar-static-top hidden-print">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <i class="fa fa-bars fa-lg"></i>
+          </button>
+        <div class="logo">
+          <a href="face_to_face.php"><img src="assets/img/logo.png" style="height:70px;"><i class="fas fas-logo"></i></a>
+        </div>
       </div>
-    </div>
 
-    <div class="navbar-collapse collapse">
-      <!-- <ul class="nav navbar-nav"> -->
-        <!-- <li class="hidden-sm "><a href="../">Home</a></li>
-        <li class="hidden-sm">
-          <a href="../whats-new/">What's New</a>
-        </li>
-        <li class="hidden-xs hidden-md hidden-lg">
-          <a href="../whats-new/">New</a>
-        </li>
-        <li><a href="../get-started/">Get Started</a></li>
-        <li class="dropdown-split-left"><a href="../icons/">Icons</a></li>
-        <li class="dropdown dropdown-split-right hidden-xs">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-            <i class="fa fa-caret-down"></i>
-          </a> -->
-          <!-- <ul class="dropdown-menu pull-right">
-            <li><a href="../icons/"><i class="fa fa-flag fa-fw"></i>&nbsp; All Icons</a></li>
-            <li class="divider"></li>
-            <li><a href="../icons/#new"><i class="fa fa-fort-awesome fa-fw"></i>&nbsp; New Icons in 4.5</a></li>
-            <li><a href="../icons/#web-application"><i class="fa fa-camera-retro fa-fw"></i>&nbsp; Web Application Icons</a></li>
-            <li><a href="../icons/#hand"><i class="fa fa-hand-spock-o fa-fw"></i>&nbsp; Hand Icons</a></li>
-            <li><a href="../icons/#transportation"><i class="fa fa-ship fa-fw"></i>&nbsp; Transportation Icons</a></li>
-            <li><a href="../icons/#gender"><i class="fa fa-venus fa-fw"></i>&nbsp; Gender Icons</a></li>
-            <li><a href="../icons/#file-type"><i class="fa fa-file-image-o fa-fw"></i>&nbsp; File Type Icons</a></li>
-            <li><a href="../icons/#spinner"><i class="fa fa-spinner fa-fw"></i>&nbsp; Spinner Icons</a></li>
-            <li><a href="../icons/#form-control"><i class="fa fa-check-square fa-fw"></i>&nbsp; Form Control Icons</a></li>
-            <li><a href="../icons/#payment"><i class="fa fa-credit-card fa-fw"></i>&nbsp; Payment Icons</a></li>
-            <li><a href="../icons/#chart"><i class="fa fa-pie-chart fa-fw"></i>&nbsp; Chart Icons</a></li>
-            <li><a href="../icons/#currency"><i class="fa fa-won fa-fw"></i>&nbsp; Currency Icons</a></li>
-            <li><a href="../icons/#text-editor"><i class="fa fa-file-text-o fa-fw"></i>&nbsp; Text Editor Icons</a></li>
-            <li><a href="../icons/#directional"><i class="fa fa-arrow-right fa-fw"></i>&nbsp; Directional Icons</a></li>
-            <li><a href="../icons/#video-player"><i class="fa fa-play-circle fa-fw"></i>&nbsp; Video Player Icons</a></li>
-            <li><a href="../icons/#brand"><i class="fa fa-github fa-fw"></i>&nbsp; Brand Icons</a></li>
-            <li><a href="../icons/#medical"><i class="fa fa-medkit fa-fw"></i>&nbsp; Medical Icons</a></li>
-          </ul> -->
-        <!-- </li> -->
-        <!-- <li class="dropdown-split-left"><a href="../examples/">Examples</a></li>
-        <li class="dropdown dropdown-split-right hidden-xs">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-            <i class="fa fa-caret-down"></i>
-          </a> -->
-          <ul class="dropdown-menu pull-right">
-            <li><a href="../examples/">Examples</a></li>
-            <li class="divider"></li>
-            <li><a href="../examples/#basic">Basic Icons</a></li>
-            <li><a href="../examples/#larger">Larger Icons</a></li>
-            <li><a href="../examples/#fixed-width">Fixed Width Icons</a></li>
-            <li><a href="../examples/#list">List Icons</a></li>
-            <li><a href="../examples/#bordered-pulled">Bordered & Pulled Icons</a></li>
-            <li><a href="../examples/#animated">Animated Icons</a></li>
-            <li><a href="../examples/#rotated-flipped">Rotated &amp; Flipped Icons</a></li>
-            <li><a href="../examples/#stacked">Stacked Icons</a></li>
-            <li><a href="../examples/#bootstrap">Bootstrap 3 Examples</a></li>
-            <li><a href="../examples/#custom">Custom CSS</a></li>
-          </ul>
-      
-        <!-- </li>
-        <li class="active"><a href="../community/">Community</a></li> -->
-        <!-- <li><a href="マイページのurl">マイページ</a></li>
-      </ul> -->
-      <div class="header-right btnk">
+        <div class="header-right btnk">
           <ul class="nav navbar-nav navbar-right" >
             <li><a href="schedule.php"><strong>Schedule</strong></a></li>
-            <!-- <li><a href="file:///Users/ryotsukasa/Documents/web201510/Marco%20Theme/index-ichiran.html"><strong>Schedule</strong></a></li> -->
             <li><a href="profile.php"><strong>Profile</strong></a></li>
           </ul>
-      </div><!-- <div class="header-right btn1"> -->
-    </div>
+        </div>
+      </div>
   </div>
-</div>
 
-
-
-    <div class="col-lg-10 col-sm-10">
+<!--     <div class="col-lg-10 col-sm-10">
         <div class="card hovercard">
             <div class="card-background">
                 <img class="card-bkimg" alt="" src="http://lorempixel.com/100/100/people/9/">
-                <!-- http://lorempixel.com/850/280/people/9/ -->
-            </div>
+ -->                <!-- http://lorempixel.com/850/280/people/9/ -->
+<!--             </div>
             <div class="useravatar">
                 <img alt="" src="http://lorempixel.com/100/100/people/9/">
             </div>
             <div class="card-info"> <span class="card-title">Pamela Anderson</span>
 
             </div>
-        </div>
+        </div> -->
 <!--         <div class="btn-pref btn-group btn-group-justified btn-group-lg" role="group" aria-label="...">
             <div class="btn-group" role="group">
                 <button type="button" id="stars" class="btn btn-primary" href="#tab1" data-toggle="tab"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>
@@ -295,6 +175,16 @@ if (!empty($_POST)){
  
         <div class="container">
             <form method="post" action="" class="text-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Category <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a href="#">learning</a></li>
+                  <li><a href="#">Traveling</a></li>
+                  <li><a href="#">Drinking</a></li>
+                </ul>
+              </div>
                 <div class="form-group form-inline">
                   <label for="name" class="control-label">Title:</label>
                   <input type="text" name="title" id="title" placeholder="Title" class="form-control">
@@ -335,13 +225,8 @@ if (!empty($_POST)){
                   <?php endif;?>
                 </div>
                 
-                <!-- <div class="form-group form-inline">
-                  <label for="name" class="control-label">Date:</label>
-                  <input type="text" name="date" id="date" placeholder="Date" class="form-control" value=
-                  
-                </div> -->
                 <div class="form-group form-inline">
-                    <input type="file" name="image" size="70"> 
+                    <input type="file" name="image" class="form-control"> 
                 </div>
 
 
